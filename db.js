7b648af1c69cb86fe0ec03600bb68d0fa827958f
Chris Aquino <chris.aquino@gmail.com>
2018-07-06 08:37:45 -0400

    moves database functionality to own file

---
+++ db.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const sqlite = require('sqlite');
+const dbConnection = sqlite.open(process.env.DB_FILE, { Promise });
+
+function selectTemperatures(sensorNumber=0, dataPoints=60) {
+  console.log('getting temperatures');
+  
+  return dbConnection.then(db => {
+    return db.all(`
+      SELECT * from 
+          (
+            select 
+                (timestamp * 1000) as x, sensnum, temp as y
+            from
+                temps
+            where
+                sensnum = ${sensorNumber}
+                and
+                timestamp >= (strftime('%s', 'now') - 3600)
+            order by x desc
+            limit ${dataPoints}
+          )
+      order by x asc;`
+    );
+    
+  });  
+}
+
+// selectTemperatures()
+  // .then((rows) => {    
+    // console.log(rows);
+    // console.log(`^^^ DATA!`);
+    // console.log(rows.length);
+  // })
+  // .catch(err => console.log)
+
+module.exports = {
+  selectTemperatures
+};
