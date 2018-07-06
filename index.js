6e9832168df20a6bee81c3f17e369531a66b73eb
Chris Aquino <chris.aquino@gmail.com>
2018-07-06 08:48:23 -0400

    echoes latest temp through websockets

--- index.js
+++ index.js
@@ -14,16 +14,24 @@ const wss = new WebSocket.Server({server});
 // Pull the port number from the env
 const PORT = process.env.PORT || 3000;
 
+const db = require('./db');
+
 // Use express' static middleware to server the HTML
 app.use(express.static('public'));
 
-// For now, simply report connections and messages
+// For now, simply report connection and echo latest temp
 wss.on('connection', ws => {
   ws.on('message', msg => {
     console.log(`received ${msg}`);
+    db.selectTemperatures()
+      .then(rows => {
+        ws.send('you want stuffs?');
+        ws.send(JSON.stringify(rows[0]));
+      })
+      .catch(console.log)
   });
   ws.send('ahoy there!');
-})
+});
 
 // Start the server.
 server.listen(PORT, () => {
