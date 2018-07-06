ea7ddd9bf8104791448ebf40676ac86bddd295e0
Chris Aquino <chris.aquino@gmail.com>
2018-07-06 09:31:30 -0400

    draws current temp on an interval

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,11 +1,25 @@
 console.log('Connecting...');
-
 const ws = new WebSocket(`ws://` + location.host);
 
+const output = document.querySelector(`[data-output]`);
+let heartbeat;
+
+const FREQ = 15000;
+
 ws.onopen = (e) => {
-  ws.send('hey');
+  ws.send('hey');    
+
+  heartbeat = setInterval(() => {
+    ws.send('hey');    
+  }, FREQ);
 
   ws.onmessage = (e) => {
-    console.log(e.data);
+    try {
+      const data = JSON.parse(e.data);
+      console.log(data.y);
+      output.textContent = data.y;
+    } catch (e) {
+      // console.error(e);
+    }
   }
 }
