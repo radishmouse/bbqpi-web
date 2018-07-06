8aea2b78bfdc527f5a9597cea31abc4e735af54c
Chris Aquino <chris.aquino@gmail.com>
2018-07-06 11:19:10 -0400

    prepares for drawing d3 graph of temps

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,23 +1,36 @@
 console.log('Connecting...');
 const ws = new WebSocket(`ws://` + location.host);
-
-const output = document.querySelector(`[data-output]`);
 let heartbeat;
-
 const FREQ = 15000;
 
+
+const graph = document.querySelector(`[data-output]`);
+const svg = d3.select(`[data-output]`);
+
+svg.append('svg')
+  .attr('height', 500)
+  .attr('width', 500)
+
+const ALL = 'all';
+const ONE = 'one';
+const _msg = (kind) => JSON.stringify({"req": kind})
+
+const msgAll = _msg(ALL);
+const msgOne = _msg(ONE);
+
+
 ws.onopen = (e) => {
-  ws.send('hey');    
+  ws.send(msgAll);    
 
   heartbeat = setInterval(() => {
-    ws.send('hey');    
+    ws.send(msgOne);    
   }, FREQ);
 
   ws.onmessage = (e) => {
     try {
       const data = JSON.parse(e.data);
-      console.log(data.y);
-      output.textContent = data.y;
+      console.log(data);
+      // output.textContent = data.y;
     } catch (e) {
       // console.error(e);
     }
