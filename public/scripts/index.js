d126a82d2ccbe47d54b181d7ab633c7f83420d43
Chris Aquino <chris.aquino@gmail.com>
2018-07-07 08:55:18 -0400

    changes to line graph

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,7 +2,10 @@ console.log('Connecting...');
 const ws = new WebSocket(`ws://` + location.host);
 let heartbeat;
 const FREQ = 15000;
-const SIZE = 500;
+const SIZE = {
+  HEIGHT: 500,
+  WIDTH: 960
+};
 const BAR = {
   WIDTH: 1
 }
@@ -15,45 +18,6 @@ const msgAll = _msg(ALL);
 const msgOne = _msg(ONE);
 
 let temperatureData = [];
-
-// const graph = document.querySelector(`[data-output]`);
-const svg = d3.select(`[data-output]`);
-
-const stage = svg.append('svg')
-  .attr('height', SIZE)
-  .attr('width', SIZE * 2)
-  .style('background', 'orange')
-  .append('g')
-
-
-
-const render = () => {
-  // console.log('doing and working and doing');
-  let x = d3.scaleTime();
-  x.domain(d3.extent(temperatureData, d => new Date(d.x)));
-  x.range(d3.extent(temperatureData, d => d.y));
-
-  // let xAxis = d3.axisBottom(x).ticks(temperatureData.length)
-  
-  // svg.append('g').call(xAxis);
-
-  let rects = stage.selectAll(`rect`).data(temperatureData);
-
-  rects.enter().append(`rect`)
-    .attr('width', BAR.WIDTH)
-    .attr('x', (d, i) => i * BAR.WIDTH * 2)
-    .attr('y', d => SIZE - d.y)
-    .attr('height', d => d.y)
-    .style('background', 'red')
-
-  // rects.transition()
-    // .duration(0)
-      // .attr('x', (d, i) => i * 6)
-      // .attr('y', d => 100 - d.y)
-      // .attr('height', d => d.y)
-
-  // rects.exit().remove();
-}
 
 ws.onopen = (e) => {
   // Initially, get everything.
@@ -74,11 +38,81 @@ ws.onopen = (e) => {
         ...data.slice(0)
       ];
 
-      console.log('hay');
+      // heartbeat = 500;
+      // setInterval(() => {
+      //   temperatureData = [
+      //     ...temperatureData,
+      //     ...data.slice(heartbeat, heartbeat + 100)
+      //   ];
+      //   render();
+        
+      // }, 100)
+      
       // output.textContent = data.y;
       render();
     } catch (e) {
       // console.error(e);
     }
   }
+}
+
+// Based on https://bl.ocks.org/d3noob/402dd382a51a4f6eea487f9a35566de0
+
+// Set the dimensions and the margins of the graph
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 30,
+  left: 50
+};
+
+const width = SIZE.WIDTH - margin.left - margin.right;
+const height = SIZE.HEIGHT - margin.top - margin.bottom;
+
+// create the time parser for unix timestamps
+const parseTime = d3.timeParse('%Q');
+
+// set the ranges based on the size of the graph
+const x = d3.scaleTime().range([0, width]);
+const y = d3.scaleLinear().range([height, 0]);
+
+// define the line in terms of my range functions (created above)
+const valueLine = d3.line()
+      .x(d => x(d.x))
+      .y(d => y(d.y))
+
+// appends the svg object to the page
+// appends a group element to the svg object
+// moves the group element to the top left margin
+const svg = d3.select(`[data-output]`).append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.right})`)
+
+const render = () => {
+
+  // Format the data
+  const data = temperatureData.map(d => ({
+    x: parseTime(d.x),
+    y: parseFloat(d.y)
+  }));
+
+  // Scale the range of the data
+  x.domain(d3.extent(data, d => d.x));
+  y.domain(d3.extent(data, d => d.y));
+
+  // Add the valueLine path
+  svg.append('path')
+    .data([data])  
+    .attr('class', 'line')
+    .attr('d', valueLine)
+
+  // Add the X Axis
+  svg.append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(d3.axisBottom(x))
+
+  svg.append('g')
+    .call(d3.axisLeft(y))
 }
